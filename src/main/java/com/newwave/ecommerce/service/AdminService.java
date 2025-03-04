89d1416e7761946eb15066398bda146d95b700dd
Quy3n <quyenngoc19102002@gmail.com>
2025-03-04 14:59:42 +0700

    update

--- src/main/java/com/newwave/ecommerce/service/AdminService.java
+++ src/main/java/com/newwave/ecommerce/service/AdminService.java
@@ -1,5 +1,10 @@
 package com.newwave.ecommerce.service;
 
+import com.newwave.ecommerce.domain.ProductDTO;
+import com.newwave.ecommerce.entity.User;
+
 public interface AdminService {
-    //String removeProductByName(String productName, User admin)
+    String removeProductByName(String productName, User admin);
+    ProductDTO addProduct(ProductDTO product, User admin);
+
 }
--- src/main/java/com/newwave/ecommerce/service/impl/AdminServiceImpl.java
+++ src/main/java/com/newwave/ecommerce/service/impl/AdminServiceImpl.java
@@ -2,45 +2,53 @@ package com.newwave.ecommerce.service.impl;
 
 import com.newwave.ecommerce.domain.ProductDTO;
 import com.newwave.ecommerce.entity.Product;
+import com.newwave.ecommerce.entity.User;
+import com.newwave.ecommerce.repository.ProductRepo;
 import com.newwave.ecommerce.service.AdminService;
 
 import java.util.Optional;
 
 public class AdminServiceImpl implements AdminService {
-//    //admin
-//    @Override
-//    public String removeProductByName(String productName) {
-//        Optional<Product> product = productRepo.findByProductName(productName);
-//
-//        if(product.isEmpty()) {
-//            return "Product not found";
-//        }
-//        try {
-//            productRepo.delete(product.get());
-//        } catch (Exception e) {
-//            return e.getMessage();
-//        }
-//        return "Product removed: "+ product.get().getProductName();
-//
-//    }
+    private final ProductRepo productRepo;
 
-//    @Override
-//    public ProductDTO addProduct(ProductDTO product) {
-//        Product productE = Product.builder()
-//                .productName(product.getProductName())
-//                .quantity(product.getQuantity())
-//                .price(product.getPrice())
-//                .imageUrl(product.getImageUrl())
-//                .build();
-//
-//        Product p = productRepo.save(productE);
-//        return ProductDTO.builder()
-//                .productName(p.getProductName())
-//                .quantity(p.getQuantity())
-//                .price(p.getPrice())
-//                .imageUrl(p.getImageUrl())
-//                .build();
-//    }
+    public AdminServiceImpl(ProductRepo productRepo) {
+        this.productRepo = productRepo;
+    }
 
+    //admin
+    @Override
+    public String removeProductByName(String productName, User admin) {
+        //todo: xác thực
+        Optional<Product> product = productRepo.findByProductName(productName);
 
+        if(product.isEmpty()) {
+            return "Product not found";
+        }
+        try {
+            productRepo.delete(product.get());
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+        return "Product removed: "+ product.get().getProductName();
+
+    }
+
+    @Override
+    public ProductDTO addProduct(ProductDTO product, User admin) {
+        //todo: xác thực
+        Product productE = Product.builder()
+                .productName(product.getProductName())
+                .quantityStock(product.getQuantityStock())
+                .price(product.getPrice())
+                .imageUrl(product.getImageUrl())
+                .build();
+
+        Product p = productRepo.save(productE);
+        return ProductDTO.builder()
+                .productName(p.getProductName())
+                .quantityStock(p.getQuantityStock())
+                .price(p.getPrice())
+                .imageUrl(p.getImageUrl())
+                .build();
+    }
 }
